40b4ff31fea841e9355189c069edbddd406fe08e
laveur2keux <toabenuts@gmail.com>
2024-10-17 23:34:01 +0200

    Même changement.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Projet par Louis-Théo WIRTH, création du doc. 15/10/24 15:30.
 
+import time, random
+
 def afficher_grille(grille, cacher=False):
     '''Transforme la grille en entrée en grille de type string.
 
@@ -79,6 +81,14 @@ def signe(nb):
         return
     
 def demander_case(str,annulable=False):
+    '''Demande la case, si elle est en dehors des positions définies, redemande
+    
+    Entrée:
+        str (string): string de la question
+        annulable (boolean optional): dit si on peut écrire 'Annuler' pour return annuler
+        
+    Sortie:
+        nom_case (string): Annuler ou position de case'''
     nom_case = input(str)
     if annulable == True:
         if nom_case == "Annuler": return "Annuler"
@@ -94,67 +104,147 @@ def demander_case(str,annulable=False):
 grille_joueurs = []
 grille_joueurs.append(generer_grille(10,10)) # Générer les grilles des joueurs.
 grille_joueurs.append(generer_grille(10,10))
+ia = int(input("Contre qui voulez vous jouer?\n0:Un autre joueur en local\n1:IA aléatoire:"))
 
 placement = True
 for joueur in range(2):
-    print(afficher_grille(grille_joueurs[joueur])) # Afficher la grille vide pour aider le joueur à se repérer.
+    if (ia != 0) and (joueur == 1):  # Si IA est activée (ia != 0) et que c'est le joueur 2 (IA), alors on continue.
+        print("Placement des bateaux")  # Indique le début du placement des bateaux pour l'IA.
+        for h in range(2,7):  # Parcourt les tailles de bateaux : 2, 3, 3, 4, 5.
+            if h < 4:
+                i = h  # Les bateaux de taille 2 ou 3.
+            else:
+                i = h-1  # Les bateaux de taille 3, 4, ou 5 (car deux bateaux de taille 3).
+            
+            condition_break = False  # Indicateur de sortie de la boucle de placement.
+            dir = random.randint(0, 1)  # Direction aléatoire du bateau : 0 = horizontal, 1 = vertical.
+            while True:  # Boucle infinie pour trouver une position valide pour placer le bateau.
+                # Sélectionne une case de départ aléatoire sur la grille en fonction de la taille du bateau.
+                case = (random.randint(0, 9-i+1), random.randint(0, 9-i+1))
+                
+                # Vérifie si la case de départ est libre (valeur "_").
+                if grille_joueurs[joueur][case[0]][case[1]] == "_":
+                    condition_break = True
+                    for j in range(i):  # Vérifie si toutes les cases adjacentes pour le bateau sont libres.
+                        if grille_joueurs[joueur][case[0]+j*dir][case[1]+j*(1-dir)] == "O":
+                            condition_break = False  # Si une case est libre, on sort de la boucle de vérification.
+                            break  # Sort de la boucle si une case est validée.
+                if condition_break == True:  # Si une position valide est trouvée, on sort de la boucle while.
+                    break
 
-    print("Placement des bateaux")
-    for h in range(2,7):
-        if h <4:
-            i = h
-        else:
-            i = h-1
+            # Place le bateau en remplissant les cases avec "O".
+            for j in range(i):  # Remplit chaque case de la taille du bateau avec "O".
+                grille_joueurs[joueur][case[0]+j*dir][case[1]+j*(1-dir)] = "O"  # Place "O" pour chaque case du bateau.
         print(afficher_grille(grille_joueurs[joueur]))
-        print("Placement du bateau de taille ",i) # Indique la taille du bateau à placer
-        
-        # Demande la première case de placement du bateau
-        nom_case = demander_case("Position 1 : Quelle case choisissez vous? Format lettre chiffre (i.e C4 H8)")
-        case = convert_case(nom_case) # Convertit l'entrée de la case en coordonnées (i.e. C4 -> (2, 3))
-        while grille_joueurs[joueur][case[0]][case[1]] != "_":
+    else:
+
+        print("Placement des bateaux")
+        for h in range(2,7): # Pour avoir les tailles de bateaux : 2,3,3,4,5
+            if h <4:
+                i = h # 2, 3
+            else:
+                i = h-1 # 3, 4, 5
+            print(afficher_grille(grille_joueurs[joueur])) # Afficher la grille vide pour aider le joueur à se repérer.
+            print("Placement du bateau de taille ",i) # Indique la taille du bateau à placer
+            
+            # Demande la première case de placement du bateau
             nom_case = demander_case("Position 1 : Quelle case choisissez vous? Format lettre chiffre (i.e C4 H8)")
-            case = convert_case(nom_case)
+            case = convert_case(nom_case) # Convertit l'entrée de la case en coordonnées (i.e. C4 -> (2, 3))
+            while grille_joueurs[joueur][case[0]][case[1]] != "_":
+                nom_case = demander_case("Position 1 : Quelle case choisissez vous? Format lettre chiffre (i.e C4 H8)")
+                case = convert_case(nom_case)
 
-        grille_joueurs[joueur][(case[0])][(case[1])] = "O" # Marque la première case choisie avec un "O" pour indiquer la présence d'un bateau
-        continuer = False # Indique si le joueur peut continuer le placement ou s'il doit encore ajuster 
+            grille_joueurs[joueur][(case[0])][(case[1])] = "O" # Marque la première case choisie avec un "O" pour indiquer la présence d'un bateau
+            continuer = False # Indique si le joueur peut continuer le placement ou s'il doit encore ajuster 
 
-        while continuer == False: # Boucle jusqu'à ce que le placement du bateau soit valide
-            print(afficher_grille(grille_joueurs[joueur]))
-            # Boucle pour gérer la deuxième position ou annuler
-            while True:
-                nom_case = demander_case(f"Position 2 : Quelle case choisissez-vous? Format lettre chiffre (i.e C4 H8), Espacée de {i} cases par rapport à {nom_case} ('Annuler' pour repositionner la 1ere case).",True)
+            while continuer == False: # Boucle jusqu'à ce que le placement du bateau soit valide
+                print(afficher_grille(grille_joueurs[joueur]))
+                # Boucle pour gérer la deuxième position ou annuler
+                while True:
+                    nom_case2 = demander_case(f"Position 2 : Quelle case choisissez-vous? Format lettre chiffre (i.e C4 H8), Espacée de {i} cases par rapport à {nom_case} ('Annuler' pour repositionner la 1ere case).",True)
 
-                if nom_case == "Annuler":
-                    # Réinitialise la première case et redemande la saisie
-                    grille_joueurs[joueur][case[0]][case[1]] = "_"  # Remet la première case à "_"
-                    nom_case = demander_case("Position 1 : Quelle case choisissez-vous? Format lettre chiffre (i.e C4 H8)")
-                    case = convert_case(nom_case)
-
-                    # Vérifie que la case est valide et vide
-                    while grille_joueurs[joueur][case[0]][case[1]] != "_":
+                    if nom_case2 == "Annuler":
+                        # Réinitialise la première case et redemande la saisie
+                        grille_joueurs[joueur][case[0]][case[1]] = "_"  # Remet la première case à "_"
                         nom_case = demander_case("Position 1 : Quelle case choisissez-vous? Format lettre chiffre (i.e C4 H8)")
                         case = convert_case(nom_case)
 
-                    # Marque la case comme occupée
-                    grille_joueurs[joueur][case[0]][case[1]] = "O"
-                    print(afficher_grille(grille_joueurs[joueur]))
+                        # Vérifie que la case est valide et vide
+                        while grille_joueurs[joueur][case[0]][case[1]] != "_":
+                            nom_case = demander_case("Position 1 : Quelle case choisissez-vous? Format lettre chiffre (i.e C4 H8)")
+                            case = convert_case(nom_case)
 
+                        # Marque la case comme occupée
+                        grille_joueurs[joueur][case[0]][case[1]] = "O"
+                        print(afficher_grille(grille_joueurs[joueur]))
+
+                    else:
+                        # Convertit la deuxième case si elle est valide
+                        case2 = convert_case(nom_case2)
+                        break  # Sort de la boucle si la deuxième case est valide
+
+                d = [(case[0]-case2[0]),(case[1]-case2[1])]  # Calcule la différence entre les deux cases choisies pour s'assurer que l'espacement est correct
+                if d[1] == 0: # Vérifie si le bateau est placé verticalement (d[1] == 0)
+                    if abs(d[0])==i-1: # Vérifie si la distance entre les deux cases correspond à la taille du bateau
+                        for j in range(i): # Remplit toutes les cases entre la case1 et case2 avec "O"
+                            grille_joueurs[joueur][(case[0]-j*signe(d[0]))][(case[1])] = "O" 
+                        continuer = True # Placement valide, sortie de la boucle
+                elif d[0] == 0: # Vérifie si le bateau est placé horizontalement (d[0] == 0)
+                    if abs(d[1])==i-1: # Vérifie si la distance entre les deux cases correspond à la taille du bateau
+                        for j in range(i): # Remplit toutes les cases entre la case1 et case2 avec "O"
+                            grille_joueurs[joueur][(case[0])][(case[1]-j*signe(d[1]))] = "O" 
+                        continuer = True # Placement valide, sortie de la boucle
                 else:
-                    # Convertit la deuxième case si elle est valide
-                    case2 = convert_case(nom_case)
-                    break  # Sort de la boucle si la deuxième case est valide
+                    print("Placement invalide.")
+            # print(afficher_grille(grille_joueurs[joueur]))
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nAutre joueur!")
 
-            d = [(case[0]-case2[0]),(case[1]-case2[1])]  # Calcule la différence entre les deux cases choisies pour s'assurer que l'espacement est correct
-            if d[1] == 0: # Vérifie si le bateau est placé verticalement (d[1] == 0)
-                if abs(d[0])==i-1: # Vérifie si la distance entre les deux cases correspond à la taille du bateau
-                    for j in range(i): # Remplit toutes les cases entre la case1 et case2 avec "O"
-                        grille_joueurs[joueur][(case[0]-j*signe(d[0]))][(case[1])] = "O" 
-                    continuer = True # Placement valide, sortie de la boucle
-            elif d[0] == 0: # Vérifie si le bateau est placé horizontalement (d[0] == 0)
-                if abs(d[1])==i-1: # Vérifie si la distance entre les deux cases correspond à la taille du bateau
-                    for j in range(i): # Remplit toutes les cases entre la case1 et case2 avec "O"
-                        grille_joueurs[joueur][(case[0])][(case[1]-j*signe(d[1]))] = "O" 
-                    continuer = True # Placement valide, sortie de la boucle
-            else:
-                print("Placement invalide.")
 print("\n\n\n\n\n\n\n\n\n\nDebut de partie!")
+
+joueur = 0
+continuer = True # Variable de contrôle pour la boucle principale, qui permet de continuer le jeu
+while continuer: # Boucle principale du jeu qui continue tant que 'continuer' est True
+    print('Tour du joueur '+str(joueur+1)+'!')
+    time.sleep(0.5)
+    while True: # Boucle qui demande au joueur de choisir une case valide pour attaquer
+        print(afficher_grille(grille_joueurs[1-joueur],True)) # Affiche la grille de l'adversaire (1-joueur) en masquant les informations cachées (True)
+        if (ia != 0) and (joueur == 1):  # Si IA est activée (ia != 0) et que c'est le joueur 1 (IA), alors on continue.
+            if ia == 1:  # Si l'IA est de niveau 1.
+                while True: # Boucle pour s'assurer que la case est valable.
+                    case = (random.randint(0,9),random.randint(0,9))
+                    if (grille_joueurs[1-joueur][case[0]][case[1]] == 'O') or (grille_joueurs[1-joueur][case[0]][case[1]] == '_'): # Condition pour sortir de la boucle.
+                        break
+        else:
+            nom_case = demander_case(f"Quelle case choisissez-vous d'attaquer? Format lettre chiffre (i.e C4 H8)")
+            case = convert_case(nom_case)
+
+        if grille_joueurs[1-joueur][case[0]][case[1]] == "O": # Si la case contient un "O" (cela signifie qu'un bateau est présent)
+            grille_joueurs[1-joueur][case[0]][case[1]] = "@" # Marque la case comme touchée avec "@"
+            print(afficher_grille(grille_joueurs[1-joueur],True))
+            print("Touché!") # Indique au joueur que le coup a touché un bateau
+            time.sleep(1) # Pause de 1 seconde pour donner du temps au joueur avant de continuer
+
+
+            sum = 0 # Initialise la variable `sum` à 0 pour compter les cases restantes contenant "O" (non touchées)
+            for i in range(len(grille_joueurs[1-joueur])): # Parcourt la grille du joueur pour vérifier s'il reste des cases contenant "O" (bateaux non touchés)
+                for j in range(len(grille_joueurs[1-joueur][i])):
+                    if grille_joueurs[1-joueur][i][j] == 'O': # Si une case contient encore "O", cela signifie qu'il reste un bateau
+                        sum +=1
+                        
+            if sum == 0: # Si `sum` est égal à 0, cela signifie qu'il n'y a plus de cases "O" (tous les bateaux sont coulés)
+                vainqueur = joueur # i pour avoir l'index du joueur. (affiche le joueur 1 à chaque fois, à régler)
+                continuer = 0 # Met fin à la partie en réglant la variable `continuer` à 0 (faux)
+            break # Sort de la boucle interne après un coup réussi (le joueur a touché un bateau)
+
+        elif grille_joueurs[1-joueur][case[0]][case[1]] == "_": # Si la case contient un "_", cela signifie que c'est une case vide (coup manqué)
+            grille_joueurs[1-joueur][case[0]][case[1]] = "X" # Marque la case comme manquée avec "X"
+            print(afficher_grille(grille_joueurs[1-joueur],True)) # Affiche la grille de l'adversaire mise à jour après le coup manqué
+            print("Manqué...") # Indique au joueur que le coup a manqué le bateau
+            time.sleep(1) 
+            break # Sort de la boucle interne après un coup manqué
+        else: # Si la case ne contient ni "O" ni "_", cela signifie qu'elle à déjà attaquée
+            print("Position invalide.")
+    joueur = 1 - joueur # Pour alterner entre les deux joueurs (joueur 0 et joueur 1)
+
+
+print("Le joueur "+str(vainqueur+1)+" gagne!")
